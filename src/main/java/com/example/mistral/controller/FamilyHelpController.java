47ccb56721600f44e2922ae05b580d67db498065
Pavel Shoplik <pavel.shoplik@yandex.ru>
2024-11-17 00:13:16 +0300

    Add test endpoint

--- src/main/java/com/example/mistral/controller/FamilyHelpController.java
+++ src/main/java/com/example/mistral/controller/FamilyHelpController.java
@@ -13,10 +13,43 @@ public class FamilyHelpController {
     @Autowired
     private ChatClient chatClient;
 
+    @GetMapping("/help/work")
+    String helpWithWork(String prompt) {
+        chatClient.prompt()
+                .system("Обьясни ответ пошагово")
+                .user("");
+        System.out.println("helpWithWork called. prompt: " + prompt);
+
+        return chatClient.prompt()
+                .user(prompt)
+                .call()
+                .chatResponse()
+                .getResult()
+                .getOutput()
+                .getContent();
+    }
+
+    @GetMapping("/activity")
+    String activityPlan(String prompt) {
+        System.out.println(chatClient.prompt()
+                .system("")
+                .user("")
+                .call().content());
+        System.out.println("activityPlan called. prompt: " + prompt);
+
+        return chatClient.prompt()
+                .user(prompt)
+                .call()
+                .chatResponse()
+                .getResult()
+                .getOutput()
+                .getContent();
+    }
+
     @GetMapping("/help")
-    String familyHelp(String prompt) {
-        System.out.println("familyHelp called. prompt: " + prompt);
-//        return "Test successful";
+    String helpFamily(String prompt) {
+        System.out.println("helpFamily called. prompt: " + prompt);
+
         return chatClient.prompt()
                 .user(prompt)
                 .call()
